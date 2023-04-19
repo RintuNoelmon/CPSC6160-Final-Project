1874ffa35dad985878939f2406f33e71af60f52f
Rintu Noelmon <124462732+RintuNoelmon@users.noreply.github.com>
2023-04-19 01:23:58 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -9,27 +9,33 @@ import screen as s
 import pygame
 #from pygame import mixer
 import welcome_screen as w
+from Level2 import Level2
+
+from settings import *
 
 
-
+FramePerSec = pygame.time.Clock()
+FPS = 180
+background_sprite = pygame.image.load('./Images/img.png')
 class Game(s.Screen):
     def run(self):
         running = False
         #game_over = False
+        clock = pygame.time.Clock()
+
         welcome_screen = True
         #mixer.music.load(r"Music/bgMusic.wav")
         #mixer.music.play(-1)
 
         #Welcome Screen 
-        
-        
+        # self.image = background_sprite
         button_img_start = pygame.image.load("Images/button.png").convert_alpha()
-        button_start = w.Button(250,300,button_img_start,0.7)
+        button_start = w.Button(600,300,button_img_start,0.7)
         button_img_exit = pygame.image.load("Images/button_exit.png").convert_alpha()  
-        button_exit = w.Button(250,450,button_img_exit,0.76)
+        button_exit = w.Button(600,450,button_img_exit,0.76)
 
         while welcome_screen == True:
-            w.Welcome_Screen.wlcm_scrn.fill((106, 159, 181))
+            w.Welcome_Screen.wlcm_scrn.fill((202, 228, 241))
             button_start.draw_btn()
             button_exit.draw_btn()
             for event in pygame.event.get():
@@ -42,26 +48,43 @@ class Game(s.Screen):
                     if btn_start == True:
                         welcome_screen = False
                         running = True
-                        break
+
+
                     if btn_exit == True:
                         pygame.quit()
                         #pygame.display.flip()
             pygame.display.flip()
-            s.Screen.clock.tick(360)
+            s.Screen.clock.tick(3600)
 
         #Main Screen
-        while running:
-            s.Screen.scrn.fill((106, 159, 181))
-            #pygame.display.flip()
+        # test_tile=pygame.sprite.Group(Tile((100,100),200))
+        level = Level2(level_map,s.Screen.scrn)
+        window = pygame.display.set_mode((1200, 700))
+        bg = pygame.transform.scale(background_sprite, (1200, 700))
+        i=0
+        while True:
+            clock.tick(180)
+            window.fill((0, 0, 0))
+            window.blit(bg, (i, 0))
+            window.blit(bg, (1200 + i, 0))
+            if (i == -1200):
+                window.blit(bg, (1200 + i, 0))
+                i = 0
+            i -= 1
+            # pygame.display.flip()
         
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-                running=False
-                
-                
-            pygame.display.flip()
-            s.Screen.clock.tick(360)
+                running=True
+
+            # s.Screen.scrn.fill((106, 159, 181))
+            # s.Screen.scrn.blits(background_sprite, (1200,700))
+            level.run()
+
+            pygame.display.update()
+            s.Screen.clock.tick(180)
+            FramePerSec.tick(FPS)
 
         pygame.quit()
